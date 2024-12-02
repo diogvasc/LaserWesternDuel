c5eda909b4d05f3e823979d97ec3835e242903e4
Diogo Vasco <65719793+diogvasc@users.noreply.github.com>
2024-12-02 17:16:59 +0000

    updated tft interface

--- src/interface/tft.ino
+++ src/interface/tft.ino
@@ -29,7 +29,7 @@ void setup() {
   
 }
 
-void drawGreen(){ // duel
+void drawDuel(){ // duel
 
   screen.stroke(0, 150, 0);  // set the stroke color to green
   screen.fill(0, 150, 0);  // set the fill color to green
@@ -41,7 +41,7 @@ void drawGreen(){ // duel
 
 }
 
-void drawRed(){ // aimlabs 
+void drawAimlabs(){ // aimlabs 
 
   screen.stroke(0, 0, 255);  // set the stroke color to red
   screen.fill(0, 0, 255);  // set the fill color to red
@@ -52,9 +52,30 @@ void drawRed(){ // aimlabs
   screen.text("AIMLABS", (screen.width() - 90 ) / 2, (screen.height() + 38) / 2);
 }
 
+void drawGamemodes(){
+  screen.stroke(0, 0, 255);  // set the stroke color to red
+  screen.fill(0, 0, 255);  // set the fill color to red
+  screen.circle(screen.width() - 135 , (screen.height()/2) - 30, 15);
+  screen.stroke(0, 0, 0);
+  screen.text("1", screen.width() - 140, screen.height()/2 - 10);
+
+  screen.stroke(0, 0, 255);  // set the stroke color to red
+  screen.fill(0, 0, 255);  // set the fill color to red
+  screen.circle(screen.width() - 30 , (screen.height()/2) - 30, 15);
+  screen.stroke(0, 0, 0);
+  screen.text("3", screen.width() - 35, screen.height()/2 - 10);
+
+  screen.stroke(0, 150, 0);  // set the stroke color to green
+  screen.fill(0, 150, 0);  // set the fill color to green
+  screen.circle(screen.width()/2 , (screen.height()/2) + 20, 15);
+  screen.stroke(0, 0, 0);
+  screen.text("2", screen.width()/2 - 5, screen.height()/2 + 40);
+
+  
+
+}
+
 void loop() {
-  drawGreen();
-  drawRed();
 }
 
 
